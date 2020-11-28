05887791a198dd571789a1612e539fd0c74c3372
SSoQw <SSoQw@SSoQw>
2020-11-27 22:55:16 -0500

    Formatting changes

--- Testing/GenericClient.java
+++ Testing/GenericClient.java
@@ -6,87 +6,86 @@ import java.io.*;
 import java.net.*;
 
 public class GenericClient {
-	
-    public static void main(String[] args) throws Exception {
-        try {
-        	Socket connectionSocket = new Socket("localhost", 25565);
-            new MessageRead(connectionSocket).start();
-            new MessageWrite(connectionSocket).start();
-        } catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        } catch (ConnectException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        } catch (SocketException ex) {
-            System.out.println("Server closed the connection");
-        }
-    }
+
+	public static void main(String[] args) throws Exception {
+		try {
+			Socket connectionSocket = new Socket("localhost", 25565);
+			new MessageRead(connectionSocket).start();
+			new MessageWrite(connectionSocket).start();
+		} catch (UnknownHostException ex) {
+			System.out.println("Server not found: " + ex.getMessage());
+		} catch (ConnectException ex) {
+			System.out.println("Server not found: " + ex.getMessage());
+		} catch (SocketException ex) {
+			System.out.println("Server closed the connection");
+		}
+	}
 
 }
 
-class MessageRead extends Thread{
+class MessageRead extends Thread {
 	Socket s;
 	BufferedReader in;
 	static String name;
-	
+
 	public MessageRead(Socket s) {
 		try {
-			 in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		}catch(IOException ex) {
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		} catch (IOException ex) {
 			System.out.println("Error with IO stream: " + ex.getMessage());
 		}
 	}
-	
-    public void run() {
-        while(true) {
-            try {
-                String message = in.readLine();
-                String[] output = message.split(": ");
-                if(output[0].contains(name + " has joined the chat!")) {
-                	System.out.print("");
-                } else if(!output[0].equals(name)) {
-                    System.out.printf("%s%n", message);    
-                }
-            } catch (IOException ex) {
-                System.out.println("Error reading: " + ex.getMessage());
-                ex.printStackTrace();
-                break;
-            } 
-        }
+
+	public void run() {
+		while (true) {
+			try {
+				String message = in.readLine();
+				String[] output = message.split(": ");
+				if (output[0].contains(name + " has joined the chat!")) {
+					System.out.print("");
+				} else if (!output[0].equals(name)) {
+					System.out.printf("%s%n", message);
+				}
+			} catch (IOException ex) {
+				System.out.println("Error reading: " + ex.getMessage());
+				ex.printStackTrace();
+				break;
+			}
+		}
 	}
 }
 
-
-class MessageWrite extends Thread{
+class MessageWrite extends Thread {
 	Socket s;
 	PrintWriter w;
-	
-	 public MessageWrite(Socket s) {
-		 try {
-          OutputStream out = new PrintStream(new BufferedOutputStream(s.getOutputStream()));
-          w = new PrintWriter(out, true);
-      } catch (IOException ex) {
-    	System.out.println("Error with IO stream: " + ex.getMessage());
-      }
+
+	public MessageWrite(Socket s) {
+		try {
+			OutputStream out = new PrintStream(new BufferedOutputStream(s.getOutputStream()));
+			w = new PrintWriter(out, true);
+		} catch (IOException ex) {
+			System.out.println("Error with IO stream: " + ex.getMessage());
+		}
 	}
-		 
-	public void run(){
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Welcome to the chatroom, please enter your name...");
-	    String name = sc.next();
-	    MessageRead.name = name;
-	    
-	    w.println(name);
-	    
-	    System.out.println("Hello " + name + "! If you ever want to leave type {quit}");
-	    
-	    String message;
-	    do {
-	    	message = sc.nextLine();
-	    	w.printf("%s:::%s%n", name, message);
-	    } while(!message.equals("{quit}"));
-	    
-	    sc.close();
-	    System.exit(0);
-	    
+
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the chatroom, please enter your name...");
+		String name = sc.next();
+		MessageRead.name = name;
+
+		w.println(name);
+
+		System.out.println("Hello " + name + "! If you ever want to leave type {quit}");
+
+		String message;
+		do {
+			message = sc.nextLine();
+			w.printf("%s:::%s%n", name, message);
+		} while (!message.equals("{quit}"));
+
+		sc.close();
+		System.exit(0);
+
 	}
 }
--- Testing/Server.java
+++ Testing/Server.java
@@ -4,87 +4,85 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
-
-
 public class Server {
 	static ServerSocket serverSocket;
-    static Vector<ClientHandler> clients = new Vector<ClientHandler>();
-    static boolean hasHost = false;
-    
-    public static void main(String[] args) throws IOException {
-        serverSocket = new ServerSocket(25565); 
-    	System.out.println("Waiting for users...");
-        
-        while (true) {
-            try {
-                Socket s = serverSocket.accept();
-                clients.add(new ClientHandler(s));
-            } catch (Exception x) {
-                System.out.println(x);
-            }
-        }
-    }
-    public static void sendToAll(String[] s) {
-    	for(ClientHandler x : clients) {
-    		if(s[1].contains("{quit}")) {
-    				x.w.printf("%s has left the game.%n", s[0]);
-    		}else if(s[1].contains("has joined the game!")){
-    				x.w.printf("%s %s%n", s[0], s[1]);
-    		}else {
-        			x.w.printf("%s: %s%n", s[0], s[1]);
-    			}
-    		}
-    	}
+	static Vector<ClientHandler> clients = new Vector<ClientHandler>();
+	static boolean hasHost = false;
+
+	public static void main(String[] args) throws IOException {
+		serverSocket = new ServerSocket(25565);
+		System.out.println("Waiting for users...");
+
+		while (true) {
+			try {
+				Socket s = serverSocket.accept();
+				clients.add(new ClientHandler(s));
+			} catch (Exception x) {
+				System.out.println(x);
+			}
+		}
+	}
+
+	public static void sendToAll(String[] s) {
+		for (ClientHandler x : clients) {
+			if (s[1].contains("{quit}")) {
+				x.w.printf("%s has left the game.%n", s[0]);
+			} else if (s[1].contains("has joined the game!")) {
+				x.w.printf("%s %s%n", s[0], s[1]);
+			} else {
+				x.w.printf("%s: %s%n", s[0], s[1]);
+			}
+		}
+	}
 }
 
 class ClientHandler extends Thread {
-	  	Socket connectionSocket;
-	    PrintStream out;
-	    PrintWriter w;
-	    public ClientHandler(Socket s) throws IOException {
-	        connectionSocket = s;
-	        start();
-	    }
+	Socket connectionSocket;
+	PrintStream out;
+	PrintWriter w;
 
-		public void run() {
-	        try {
-	        	boolean isHost = false;
-	        	
-	        	if(!Server.hasHost) {
-	        		isHost = true;
-	        		Server.hasHost = false;
-	        	}
+	public ClientHandler(Socket s) throws IOException {
+		connectionSocket = s;
+		start();
+	}
 
-		        boolean done = false;
-		        out = new PrintStream(new BufferedOutputStream(connectionSocket.getOutputStream()));
-		        w = new PrintWriter(out, true);
-		        BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		        
-		        String message;
-		        String[] messageSplit;
-		        String str = in.readLine();
-		        System.out.println(str + " has joined the game!");
-		        
-		        String[] welcome =  new String[2];
-		        welcome[0] = str;
-		        welcome[1] = "has joined the game!";
-		        Server.sendToAll(welcome);
+	public void run() {
+		try {
+			boolean isHost = false;
 
-		        
-		        while(!done){
-		        	message = in.readLine();
-		        	if(message == null) {
-		        		done = true;
-		        		continue;
-		        	} 
-		            messageSplit = message.split(":::");
-		            if(messageSplit.length>1) {
-		            	Server.sendToAll(messageSplit);
-		            }
-		        }
-	        } catch (IOException x) {
-	            System.out.println(x);
-	        }   
-	    }
+			if (!Server.hasHost) {
+				isHost = true;
+				Server.hasHost = false;
+			}
+
+			boolean done = false;
+			out = new PrintStream(new BufferedOutputStream(connectionSocket.getOutputStream()));
+			w = new PrintWriter(out, true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+			String message;
+			String[] messageSplit;
+			String str = in.readLine();
+			System.out.println(str + " has joined the game!");
+
+			String[] welcome = new String[2];
+			welcome[0] = str;
+			welcome[1] = "has joined the game!";
+			Server.sendToAll(welcome);
+
+			while (!done) {
+				message = in.readLine();
+				if (message == null) {
+					done = true;
+					continue;
+				}
+				messageSplit = message.split(":::");
+				if (messageSplit.length > 1) {
+					Server.sendToAll(messageSplit);
+				}
+			}
+		} catch (IOException x) {
+			System.out.println(x);
+		}
+	}
 }
