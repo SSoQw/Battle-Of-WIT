4c7bb3348d1888a5701de8ac1fc5cb8364c5431d
SSoQw <SSoQw@SSoQw>
2020-11-28 00:25:59 -0500

    Added setup skeleton.

--- Testing/Server.java
+++ Testing/Server.java
@@ -34,37 +34,37 @@ public class Server {
 			}
 		}
 	}
-	
+
 	public static void arithmeticGenerator(String Op) {
 		Random rand = new Random();
 		int num1;
 		int num2;
 		int solution;
-		
-		switch(Op) {
+
+		switch (Op) {
 		case "+":
-			num1 = rand.nextInt(100);
-			num2 = rand.nextInt(100);
-			solution = num1+num2;
-		break;
-		
+			num1 = rand.nextInt(100) + 1;
+			num2 = rand.nextInt(100) + 1;
+			solution = num1 + num2;
+			break;
+
 		case "-":
-			num1 = rand.nextInt(100);
-			num2 = rand.nextInt(100);
-			solution = num1-num2;			
-		break;
-		
+			num1 = rand.nextInt(100) + 1;
+			num2 = rand.nextInt(100) + 1;
+			solution = num1 - num2;
+			break;
+
 		case "*":
-			num1 = rand.nextInt(20);
-			num2 = rand.nextInt(20);
-			solution = num1*num2;
-		break;
-		
+			num1 = rand.nextInt(20) + 1;
+			num2 = rand.nextInt(20) + 1;
+			solution = num1 * num2;
+			break;
+
 		case "%":
-			num1 = rand.nextInt(400);
-			num2 = rand.nextInt(20);
-			solution = num1%num2;
-		break;
+			num1 = rand.nextInt(400) + 1;
+			num2 = rand.nextInt(20) + 1;
+			solution = num1 % num2;
+			break;
 		}
 	}
 }
@@ -92,16 +92,87 @@ class ClientHandler extends Thread {
 			out = new PrintStream(new BufferedOutputStream(connectionSocket.getOutputStream()));
 			w = new PrintWriter(out, true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
+			
 			String message;
 			String[] messageSplit;
 			String str = in.readLine();
+			
 			System.out.println(str + " has joined the game!");
 
 			String[] welcome = new String[2];
 			welcome[0] = str;
 			welcome[1] = "has joined the game!";
 			Server.sendToAll(welcome);
+			
+			if(isHost) {
+				w.write("Welcome "+ str + "! You are the host of the game. Please follow the instructions to get your game setup.");
+				w.write("First, would you like to play with Random Aritmetic, Choose a Preset, or Use Custom questions?");
+				w.write("Enter which mode you want to play (Random, Preset, Custom) case insensitive: ");
+				String gameMode = in.readLine().toLowerCase();
+				
+				while(!gameMode.equals("random") && !gameMode.equals("preset") && !gameMode.equals("custom")) {
+					w.write("No valid gamemode slected, please enter which mode you want to play (Random, Preset, Custom) case insensitive:");
+					gameMode = in.readLine().toLowerCase();
+				}
+				switch(gameMode) {
+					case "random":
+						w.write("Please enter the operation you would like to use for your aritmetic set (+, -, *, %)");
+						String Operation = in.readLine(); 
+						
+						while(!Operation.equals("+") && !Operation.equals("-") && !Operation.equals("*") && !Operation.equals("%")) {
+						w.write("Please enter enter a valid operation(+, -, *, %)");
+						Operation = in.readLine(); 
+						}
+						
+						switch(Operation) {
+						case "+":
+							
+						break;
+						
+						case "-":
+							
+						break;
+						
+						case "*":
+							
+						break;
+						
+						case "%":
+							
+						break;
+						}
+					break;
+					
+					case "preset":
+						w.write("Would you like to use the computer science set, or math set?");
+						w.write("Please entere math or compsci, case insesitive: ");
+						String set = in.readLine().toLowerCase();
+						while(!set.equals("math") && !set.equals("compsci")) {
+							w.write("No valid set selected, please entere math or compsci, case insesitive: ");
+							set = in.readLine().toLowerCase();
+						}
+						switch(set) {
+						case "math":
+						
+						break;
+						
+						case "compsci":
+						
+						break;
+						}
+					break;
+					
+					case "custom":
+						w.write("If you haven't already, add your csv file with your question/answer set to the file directory, then restart the server.");
+						w.write("If you have already added the file, please enter the name of the file, otherwise enter quit to restrat.");
+						String file = in.readLine();
+						if(file.toLowerCase().equals("quit")) {
+							System.exit(0);
+						}
+					break;
+				}
+			}
+		
 
 			while (!done) {
 				message = in.readLine();
